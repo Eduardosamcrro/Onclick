273983a6285b81c3cd3ecb4d9972c9079ce51dc4
Eduardo de Oliveira cunha <97200080+Eduardosamcrro@users.noreply.github.com>
2024-01-18 16:32:58 -0300

    Add files via upload

---
+++ Onclick/script.js
@@ -0,0 +1,7 @@
+function carConteudo() {
+    document.getElementById("Conteudo").innerHTML = "<p>Na Filosofia, aprendemos de vários sábios, que a verdadeira mudança, profunda, duradoura e, portanto, eficaz, é a mudança realizada através da Educação.</p><p> Não somente pela educação informativa, a que impera em nosso mundo atual, pois educar não consiste simplesmente em “colocar” algo na cabeça das pessoas; não consiste no acúmulo de informações e notícias, mas no ato de formar a personalidade humana através do conhecimento</p><p></></>Educar é “tirar de dentro” o que de melhor cada ser humano possui em potencial, em estado latente. Desenvolver sua capacidade de ser Bom, Justo e Generoso.</p>";
+};
+
+function carImagem() {
+    document.getElementById("Imagem").innerHTML = "<img src='os-problemas-existem-e-nada-e-facil-mas-com-forca-e-determinacao-kRg8Z-cxl.webp'>";
+}
